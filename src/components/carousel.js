1ccdac114050d6a3253dd6d0cfa846185020d9df
Leonard Doepp <l.doepp@googlemail.com>
2019-05-06 16:58:17 +0200

    Update carousel.js

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -12,23 +12,28 @@ import {
 let items = [
   {
     src: "/images/G1.jpg",
-    altText: "Guatemala"
+    altText: "Guatemala",
+    captionText: "2018"
   },
   {
     src: "/images/G2.jpg",
-    altText: "Guatemala"
+    altText: "Guatemala",
+    captionText: "2018"
   },
   {
     src: "/images/G3.jpg",
-    altText: "Guatemala"
+    altText: "Guatemala",
+    captionText: "2018"
   },
   {
     src: "/images/G4.jpg",
-    altText: "Guatemala"
+    altText: "Guatemala",
+    captionText: "2018"
   },
   {
     src: "/images/G5.jpg",
-    altText: "Guatemala"
+    altText: "Guatemala",
+    captionText: "2018"
   },
   {
     src: "/images/N1.jpg",
@@ -36,55 +41,68 @@ let items = [
   },
   {
     src: "/images/N2.jpg",
-    altText: "Namibia"
+    altText: "Namibia",
+    captionText: "2018"
   },
   {
     src: "/images/N3.jpg",
-    altText: "Namibia"
+    altText: "Namibia",
+    captionText: "2018"
   },
   {
     src: "/images/N4.jpg",
-    altText: "Namibia"
+    altText: "Namibia",
+    captionText: "2018"
   },
   {
     src: "/images/N5.jpg",
-    altText: "Namibia"
+    altText: "Namibia",
+    captionText: "2018"
   },
   {
     src: "/images/Ma1.jpg",
-    altText: "Malawi"
+    altText: "Malawi",
+    captionText: "2018"
   },
   {
     src: "/images/Ma2.jpg",
-    altText: "Malawi"
+    altText: "Malawi",
+    captionText: "2018"
   },
   {
     src: "/images/Ma3.jpg",
-    altText: "Malawi"
+    altText: "Malawi",
+    captionText: "2018"
   },
   {
     src: "/images/Nz1.jpg",
-    altText: "New Zealand"
+    altText: "New Zealand",
+    captionText: "2018"
   },
   {
     src: "/images/Nz2.jpg",
-    altText: "New Zealand"
+    altText: "New Zealand",
+    captionText: "2018"
   },
   {
     src: "/images/Nz3.jpg",
-    altText: "New Zealand"
+    altText: "New Zealand",
+    captionText: "2018"
   },
   {
     src: "/images/M1.jpg",
-    altText: "Mexico"
+    altText: "Mexico",
+    captionText: "2018"
   },
   {
     src: "/images/M2.jpg",
-    altText: "Mexico"
+    altText: "Mexico",
+    captionText: "2018"
   },
   {
     src: "/images/M3.jpg",
-    altText: "mexico"
+    altText: "Mexico",
+    captionText: "2018"
   }
 ];
 
@@ -142,8 +160,8 @@ class Slide extends Component {
         >
           <img src={item.src} alt={item.altText} />
           <CarouselCaption
-            captionText={item.caption}
             captionHeader={item.altText}
+            captionText={item.captionText}
           />
         </CarouselItem>
       );
